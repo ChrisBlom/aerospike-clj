b130ebd89983628c1626674457bdb1248d371556
Chris Blom <chris.blom@millmobile.com>
2017-11-16 09:58:55 +0100

    add jitpack repo to project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "http://github.com/ChrisBlom/clj-aerospike"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :repositories [["jitpack" "https://jitpack.io"]]
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.aerospike/aerospike-client "3.2.5"]]
 
